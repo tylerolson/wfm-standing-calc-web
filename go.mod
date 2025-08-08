ed31338bddab2e3d668e2b0ebd1c923df02257d2
Tyler Olson <tolson519@gmail.com>
2025-08-07 21:39:25 -0700

    basic templ site

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tylerolson/wfm-standing-calc-web
+
+go 1.24.6
+
+require (
+	github.com/a-h/templ v0.3.924 // indirect
+	github.com/tylerolson/wfm-plat-efficiency v0.2.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/a-h/templ"
+	wfmplatefficiency "github.com/tylerolson/wfm-plat-efficiency"
+)
+
+func updateAllVendors(scraper *wfmplatefficiency.Scraper) {
+	fmt.Println("Starting all vendors at: ", time.Now())
+	for name := range scraper.GetVendors() {
+		resultChan, err := scraper.UpdateVendorStats(name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Starting %v\n", name)
+		for value := range resultChan { // Loop until the channel is closed
+			if value.Err != nil {
+				fmt.Printf("Failed to fetch %s: %v\n", value.ItemName, value.Err)
+			} else {
+				fmt.Printf("Fetched %v\n", value.ItemName)
+			}
+		}
+	}
+	fmt.Println("Done at", time.Now())
+}
+
+func main() {
+	scraper := wfmplatefficiency.NewScraper()
+	scraper.LoadVendors()
+
+	ticker := time.NewTicker(5 * time.Hour)
+	defer ticker.Stop()
+
+	go func() {
+		updateAllVendors(scraper)
+		for range ticker.C {
+			updateAllVendors(scraper)
+		}
+	}()
+
+	index := index(scraper.GetVendors())
+	http.Handle("/", templ.Handler(index))
+
+	fmt.Println("Listening on :3000")
+	http.ListenAndServe(":3000", nil)
+}
