4f515bc1b889e45eda8d33b401e892ed6e5215d0
Tyler Olson <tolson519@gmail.com>
2025-12-27 21:08:51 -0800

    remove the hook redirect, relies on some other proxy for the /api route, added nginx.conf example for the compose file

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import "./layout.css";
-  import favicon from "$lib/assets/icons8-warframe.svg";
+  import favicon from "$lib/assets/favicon.svg";
 
   let { children } = $props();
 </script>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+
   server: {
     host: "0.0.0.0",
+    proxy: {
+      "/api": "http://localhost:8080",
+      "/docs": "http://localhost:8080",
+    },
   },
 });
